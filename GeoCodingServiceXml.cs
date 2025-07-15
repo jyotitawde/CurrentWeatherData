185e7e68188d7020b09bbda6b443f6c4f799ddba
jyotitawde <70187247+jyotitawde@users.noreply.github.com>
2025-07-16 01:37:30 +0800

    Create GeoCodingServiceXml.cs
    Service class to retrieve longitude and latitude of the entered/selected city and country using OpenWeatherMap GeoCoding API

---
+++ GeoCodingServiceXml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+public class GeoCodingServiceXml
+{
+    private const string ApiKey = "YOUR_API_KEY";
+
+    public async Task<(double lat, double lon)> GetCoordinatesAsync(string city)
+    {
+         	try
+        	{
+                string url = $"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={ApiKey}&mode=xml";
+            		using var client = new HttpClient();
+            		var xml = await client.GetStringAsync(url);
+    
+           		 var serializer = new XmlSerializer(typeof(Locations));
+            		using var reader = new StringReader(xml);
+           		 var result = (Locations?)serializer.Deserialize(reader);
+            		var location = result?.LocationList?.FirstOrDefault();
+            		return location != null ? (location.Latitude, location.Longitude) : (0, 0);
+        	}
+        	catch
+       	  {
+                return (0, 0);
+       	 }
+    }
+}
